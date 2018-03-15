c3cea50ffb5dc4cd27f023bdfa2c5daa612bd563
Sébastien Gilabert <gilabert.seb@gmail.com>
2018-03-15 20:44:06 +0100

    Initial Commit

---
+++ Eden Team Builder/DataViewController.swift
@@ -0,0 +1,34 @@
+//
+//  DataViewController.swift
+//  Eden Team Builder
+//
+//  Created by Sébastien Gilabert on 15/03/2018.
+//  Copyright © 2018 Sébastien Gilabert. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
